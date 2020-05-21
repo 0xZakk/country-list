5533e09c2d6617236fb2efe20beecdb7c4d8804e
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2020-05-21 14:48:27 -0400

    add static build

---
+++ build/precache-manifest.011b86c24cdb6a0fb0c783da7e66d250.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa6f13b5acd105c0a33c",
+    "url": "/static/js/main.fa6f13b5.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "fa6f13b5acd105c0a33c",
+    "url": "/static/css/main.5fca3065.chunk.css"
+  },
+  {
+    "revision": "563bf2a1cfdb37651592f82d6cb487d5",
+    "url": "/index.html"
+  }
+];
